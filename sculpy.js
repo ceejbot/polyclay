8b7cacf01e670b7ea6fbfd613ab69754df93e664
C J Silverio <ceejceej@gmail.com>
2012-02-11 13:39:36 -0800

    Here, have a collection class. And have some basic tests for it. And have a common subclass. I'm sure all of this is so horribly ad-hoc as to turn your hair white. For "ad-hoc", read "HACK HACK HACK".

--- sculpy.js
+++ sculpy.js
@@ -1,6 +1,7 @@
 var vv = require('valentine');
 var bean = require('bean');
 var ender = $.noConflict(); // return '$' back to its original owner
+// assumes you already have ICanHaz loaded. That requires jquery.
 
 var Sculpy = function(){};
 
@@ -8,22 +9,23 @@ Sculpy.Model = function()
 {
 };
 
-Sculpy.Model.extend = function(options)
+Sculpy.Model.extend = function(options, methods)
 {
 	var sub = function()
 	{
 		this.construct();
 		if (this.initialize !== undefined)
-			this.initialize();
+			this.initialize.apply(this, arguments);
 	};
 
 	sub.prototype = new Sculpy.Model;
 	sub.prototype.constructor = sub;
-	sub.prototype.properties = [];
-	sub.prototype.calculated = [];
-	sub.prototype.defaults = {};
-	sub.prototype.initialize = options.initialize;
-	
+	sub.prototype.__urlroot = options.urlroot;
+	sub.prototype.__template = options.template;
+	sub.prototype.__properties = [];
+	sub.prototype.__calculated = [];
+	sub.prototype.__defaults = {};
+
 	var props = options.properties;
 	vv.each(props, function(k)
 	{
@@ -34,9 +36,17 @@ Sculpy.Model.extend = function(options)
 	{
 		Sculpy.Model.addCalculatedProperty(sub, k);
 	});
-	
-	sub.prototype.urlroot = options.urlroot;
-	
+
+	vv.each(Sculpy.Common.prototype, function(k)
+	{
+		sub.prototype[k] = Sculpy.Common.prototype[k];
+	});
+
+	vv.each(methods, function(k)
+	{
+		sub.prototype[k] = methods[k];
+	});
+
 	return sub;
 };
 
@@ -51,14 +61,14 @@ Sculpy.Model.prototype.construct = function()
 
 Sculpy.Model.addProperty = function(obj, name, defaultVal)
 {
-	obj.prototype.properties.push(name);
-	obj.prototype.defaults[name] = defaultVal;
+	obj.prototype.__properties.push(name);
+	obj.prototype.__defaults[name] = defaultVal;
 	Sculpy.Model.makeGetterSetter(obj, name);
 };
 
 Sculpy.Model.addCalculatedProperty = function(obj, name)
 {
-	obj.prototype.calculated.push(name);
+	obj.prototype.__calculated.push(name);
 	Sculpy.Model.makeGetterSetter(obj, name);
 };
 
@@ -71,7 +81,7 @@ Sculpy.Model.makeGetterSetter = function(obj, propname)
 		if (arguments.length == 0)
 		{
 			if (this.attributes[propname] === undefined)
-				return this.__proto__.defaults[propname];
+				return this.__defaults[propname];
 			else
 				return this.attributes[propname];
 		}
@@ -90,31 +100,37 @@ Sculpy.Model.makeGetterSetter = function(obj, propname)
 	obj.prototype[propname] = result;
 };
 
+Sculpy.Model.prototype.template = function()
+{
+	// override to return something different
+	return this.__template;
+};
+
+Sculpy.Model.prototype.render = function(tmpl)
+{
+	if (tmpl === undefined)
+		tmpl = this.template();
+	return ich[tmpl](this.toJSON());
+};
+
 Sculpy.Model.prototype.toJSON = function()
 {
 	return this.attributes;
 };
 
-Sculpy.Model.prototype.watch = function(target, event, callback)
-{
-	bean.add(target, event, callback);
-};
-
-Sculpy.Model.prototype.fire = function(event)
-{
-	bean.fire(this, event);
-};
-
 Sculpy.Model.prototype.root = function()
 {
-	return this.__proto__.urlroot;
+	if (arguments.length == 1)
+		this.__urlroot = arguments["0"];
+	else
+		return this.__urlroot;
 };
 
 Sculpy.Model.prototype.constructURL = function()
 {
 	if (this.id === undefined)
 		return this.root();
-	
+
 	return this.root() + '/' + this.id;
 }
 
@@ -125,7 +141,7 @@ Sculpy.Model.prototype.update = function(attr, silent)
 	var events = ['change'];
 	for (k in attr)
 	{
-		if (attr.hasOwnProperty(k) && (self.__proto__.properties.indexOf(k) >= 0))
+		if (attr.hasOwnProperty(k) && (self.__properties.indexOf(k) >= 0))
 		{
 			self.__proto__[k].call(self, attr[k], true);
 			events.push('change:'+k);
@@ -148,14 +164,14 @@ Sculpy.Model.prototype.save = function()
 	if (arguments.length == 1)
 		proplist = arguments["0"];
 	else
-		proplist = self.__proto__.properties;
+		proplist = self.__properties;
 
 	var props = {};
 	for (var i=0, len=proplist.length; i<len; i++)
 		props[proplist[i]] = self.attributes[proplist[i]];
 
 	var method = (self.id === undefined) ? 'POST' : 'PUT';
-	
+
 	var winning = function(data, textStatus, jqXHR)
 	{
 		self.dirty = false;
@@ -164,12 +180,12 @@ Sculpy.Model.prototype.save = function()
 		if (data.id !== undefined)
 			self.id = data.id;
 	};
-	
+
 	var losing = function(jqXHR, textStatus, errorThrown)
 	{
 		// TODO
 	};
-	
+
 	$.ajax(
 	{
 		url: self.constructURL(),
@@ -187,12 +203,12 @@ Sculpy.Model.prototype.destroy = function(success, failure)
 	{
 		self.fire('destroy');
 	};
-	
+
 	var losing = function(jqXHR, textStatus, errorThrown)
 	{
 		// TODO
 	};
-	
+
 	$.ajax(
 	{
 		url: this.constructURL(),
@@ -212,7 +228,7 @@ Sculpy.Model.prototype.load = function(success, failure)
 		self.update(data);
 		self.fire('load');
 	};
-	
+
 	var losing = function(jqXHR, textStatus, errorThrown)
 	{
 		// TODO
@@ -231,3 +247,111 @@ Sculpy.Model.prototype.load = function(success, failure)
 
 Sculpy.Collection = function(){};
 
+Sculpy.Collection.extend = function(options, methods)
+{
+	var sub = function()
+	{
+		this.construct();
+		if (this.initialize !== undefined)
+			this.initialize.apply(this, arguments);
+	};
+
+	sub.prototype = new Sculpy.Collection;
+	sub.prototype.constructor = sub;
+	sub.prototype.__urlroot = options.urlroot;
+	sub.prototype.__model = options.model;
+
+	vv.each(Sculpy.Common.prototype, function(k)
+	{
+		sub.prototype[k] = Sculpy.Common.prototype[k];
+	});
+	vv.each(methods, function(k)
+	{
+		sub.prototype[k] = methods[k];
+	});
+
+	return sub;
+};
+
+Sculpy.Collection.prototype.construct = function()
+{
+	this.__items = [];
+};
+
+Sculpy.Collection.prototype.push = function(item, silent)
+{
+	this.__items.push(item);
+	if (!silent) this.fire('add');
+};
+
+Sculpy.Collection.prototype.unshift = function(item, silent)
+{
+	this.__items.unshift(item);
+	if (!silent) this.fire('add');
+};
+
+Sculpy.Collection.prototype.reset = function(data, silent)
+{
+	var item;
+	this.__items.clear();
+	for (var i=0,len=data.length; i<len; i++)
+	{
+		item = this.__model();
+		item.update(data);
+		this.__items.push(item);
+	}
+	if (!silent) this.fire('reset');
+};
+
+Sculpy.Collection.prototype.remove = function(item, silent)
+{
+	var idx = this.__items.indexOf(item);
+	this.__items.splice(idx, 1);
+	if (!silent) this.fire('remove');
+};
+
+Sculpy.Collection.prototype.insert = function(item, index, silent)
+{
+	this.__items.splice(idx, 0, item);
+	if (!silent) this.fire('add');
+};
+
+Sculpy.Collection.prototype.url = function()
+{
+	if (arguments.length == 1)
+		this.__url = arguments["0"];
+	else
+		return this.__url;
+};
+
+Sculpy.Collection.prototype.element = function()
+{
+	if (arguments.length == 1)
+		this.__element = arguments['0'];
+	else
+		return this.__element;
+};
+
+Sculpy.Collection.prototype.render = function()
+{
+	var into = this.element();
+	if (into === undefined) return;
+	$(into).empty();
+	for (var i=0, len=this.__items.length; i<len; i++)
+	{
+		$(into).append(this.__items[i].render());
+	}
+};
+
+// methods both prototypes have in common
+Sculpy.Common = function(){};
+
+Sculpy.Common.prototype.watch = function(target, event, callback)
+{
+	bean.add(target, event, callback);
+};
+
+Sculpy.Common.prototype.fire = function(event)
+{
+	bean.fire(this, event);
+};
